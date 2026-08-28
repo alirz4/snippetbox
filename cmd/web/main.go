0048f79563a91fbdc71694b402401b82918737a9
Alireza Keshavarz <keshavrz.alirza@gmail.com>
2026-08-28 19:40:31 +0200

    Create Snippet data model and wrap database connection for handler access

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,11 +7,14 @@ import (
 	"net/http"
 	"os"
 
+	"snippetbox/internal/models"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -31,7 +34,8 @@ func main() {
 	defer db.Close()
 
 	app := &application{
-		logger: logger,
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	logger.Info("Starting server", "addr", *addr)
